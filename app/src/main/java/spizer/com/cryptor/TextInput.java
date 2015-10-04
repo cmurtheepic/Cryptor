623a9f5bffa465747e4053f8448ac6ce707ea020
Connor Murry <cmurtheepic@gmail.com>
2015-10-04 00:42:42 -0500

    i dont know what i should put as summary to here is this.

--- app/src/main/java/spizer/com/cryptor/TextInput.java
+++ app/src/main/java/spizer/com/cryptor/TextInput.java
@@ -1,14 +1,23 @@
 package spizer.com.cryptor;
 
+import android.annotation.TargetApi;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import java.lang.annotation.Target;
 
 public class TextInput extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,17 @@ public class TextInput extends AppCompatActivity {
         SQLiteDatabase database = helper.getWritableDatabase();
 
         insertText("New text");
+
+        Cursor cursor = getContentResolver().query(TextProvider.CONTENT_URI,
+                DBOpenHelper.ALL_COLUMNS, null, null, null, null);
+        String[] from = {DBOpenHelper.USER_TEXT};
+        int[] to = {android.R.id.text1};
+        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1, cursor, from, to, 0);
+
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
     }
 
     private void insertText(String userText) {
